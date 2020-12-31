8b67e5df50061eb753d1bdb02809123877366dc3
Your Name <you@example.com>
2020-12-31 03:15:55 -0300

    actualización Css

--- index.php
+++ index.php
@@ -3,25 +3,26 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>VETERINARIA JUMANJI</title>
+    <title>VETERINARIA</title>
     <link href="Css/index.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
-    <h1 id= "personalizado"> Veterinaria Jumanji </h1>
+    <h1 class= "personalizado"> Veterinaria<h2 class= "personalizado"> Spunky <h2> </h1>
+    
     <div class=boton>
-    <a href="pagmascota.php">Mascotas</a> 
+    <a href="pagmascota.php"><b>Mascotas</b></a> 
     </div>
     <div class=boton>
-    <a href="pagpropietario.php">Propietarios</a> 
+    <a href="pagpropietario.php"><b>Propietarios</b></a> 
      </div>
      <div class=boton>
-    <a href="pagespecie.php">Especies</a> 
+    <a href="pagespecie.php"><b>Especies</b></a> 
     </div> 
     <div class=boton>
-    <a href="pagraza.php">Razas</a> 
+    <a href="pagraza.php"><b>Razas</b></a> 
     </div>
     <div class=boton>
-    <a href="salir.php">Salir</a>
+    <a href="salir.php"><b>Salir</b></a>
     </div>
     </body>
 </html>
